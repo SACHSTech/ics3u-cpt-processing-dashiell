99c71d2258b81ae39b04a29ed53cc6e19148b614
Dashy88 <95883387+Dashy88@users.noreply.github.com>
2022-01-27 12:21:13 -0500

    enter

--- Sketch.java
+++ Sketch.java
@@ -1,22 +1,21 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+
 	
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * 
    */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+	// size call
+    size(624, 351);
   }
 
   /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
+   * backround
    */
   public void setup() {
-    background(210, 255, 173);
+    background(0);
   }
 
   /**
