775de6a75232d87c3b70de57a94e5ccafedce6fe
Dashy88 <95883387+Dashy88@users.noreply.github.com>
2022-01-28 00:50:29 -0500

    commit

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
 import processing.core.PApplet;
+
 /**
  * Main class to execute sketch
- * @ Dashiell Pinter
+ * @author Dashiell P
  *
  */
 class Main {
   public static void main(String[] args) {
     
     String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch(); 
+	 Sketch mySketch = new Sketch();  //comment this out to run the other sketch files
+	// Sketch1 mySketch = new Sketch1();  // uncomment this to run this sketch file
+	// Sketch2 mySketch = new Sketch2();  // uncomment this to run this sketch file
+
+
+
+	  
 	  PApplet.runSketch(processingArgs, mySketch);
   }
   
