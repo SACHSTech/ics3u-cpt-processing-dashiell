9d31c9f768e44ffcbb0395622c803f662292844c
Dashy88 <95883387+Dashy88@users.noreply.github.com>
2022-01-28 02:46:47 -0500

    commit

--- Sketch.java
+++ Sketch.java
@@ -1,8 +1,7 @@
 import processing.core.PApplet;
-;
 
 public class Sketch extends PApplet {
-
+  
   float[] circleY = new float[50];
   float[] snowfall = new float[50];
   float[] mousePositionsX = new float[25];
@@ -10,132 +9,41 @@ public class Sketch extends PApplet {
   int snowSpeed = 3;
   int num = 25;
   int index = 0;
-  
-  //scoreboard
-  int score = 0;
-
-  boolean menuScreen;
-  boolean instructionsScreen;
-  boolean Level1;
-  boolean winScreen;
-  boolean gameOverScreen;
-
+ 
   public void settings() {
     size(400, 400);
   }
-
+ 
   public void setup() {
-    menuScreen = true;
-    background(3, 219, 252);
-  }
-
-  public void draw(){
-    if(menuScreen){
-        menuScreen();
+  
+   background(0);
+    for (int i = 0; i < 50; i++) {
+      circleY[i] = random(0, 400);
     }
-    else if(Level1){
-        Level1();
+    for (int i = 0; i < 50; i++) {
+      snowfall[i] = 5;}
     }
-     else if(instructionsScreen){
-        instructionsScreen();
-     }
-    else if(winScreen){
-       winScreen();
-    }
-    else if(gameOverScreen){
-        gameOverScreen();
-    }
-  }
-
-
-    public void menuScreen(){
+  
+   public void draw() {
+ 
         background(0);
-        fill(77);
-    }
-
-      // Start Button
-      fill(255, 255, 255);
-      rect(225, 100, 300 , 75);
-  
-      // How To Play Sign
-      fill(50);
-      textSize(40);
-      text("Press 'e' to play", 235, 150);
-  
-      // How To Play Button
-      fill(255, 255, 255);
-      rect(225, 200, 300, 75);
-  
-      // Play Sign
-      fill(50);
-      textSize(28);
-      text("Press 'i' for instructions", 230, 250);
-
-        if(key == 'i'){
-            menu screen = false;
-            instructionScreen = true;
-        }
-    }
-
-    public void instructionScreen(){
-        background(255, 158, 158);
-        // Instruction Title
-        fill(0);
-        textSize(50);
-        text("Instructions", 260, 75);
-
-        //Instructions
-        fill(0);
-        textsize(35);
-        text(try to avoid the falling snowflakes!);
-        text(the more snowflakes avoided the more points you gain!);
-        text(if you hit a snowflake you lose!);
-        text(press m to go back to menu);
-
-        if key == 'm'){
-            instructionsScreen = false;
-            menuScreen = true;
-        }
-    }
-
-    public void gameOverScreen(){
-        background(0);
-        fill(250);
-        textSize(60);
-        text("Game Over", 210, 200);
-        textSize(35);
-        text("Press 'm' to go back to menu", 220, 400);
-        if(key == 'm'){
-
-            
-        }
-
-
-
-    }
-
-
-
-    }
-
-
-
-    }
-
-    }
-    
-
-  
-  
-    
-
-
-
-
+   
+  // Trail
+  background(32);
+  ellipse(mouseX, mouseY, 15, 15);
+ 
+// Drawing falling snow
+for (int circleX = 0; circleX < 50; circleX++) {
+ ellipse(circleX*8, circleY[circleX], 8, 4);
 }
-
-
+ 
+for (int i = 0; i < 50; i++) {
+ circleY[i] += snowSpeed;
+ if (circleY[i] >= 400) {
+   circleY[i] = 0;
+   snowfall[i] += 3;}
 
  }
-}
+
  }
+   }
